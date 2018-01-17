a21c767401f2836e5e80fb531a5dfe8810d9d9d1
EthanJanecek <ethanjanecek@gmail.com>
2018-01-16 20:12:45 -0600

    Add files via upload
    done

---
+++ HexPracticeLate2017/src/OI.cpp
@@ -0,0 +1,36 @@
+#include "OI.h"
+
+#include <WPILib.h>
+
+OI::OI():
+	// Process operator interface input here.
+	stick(0),
+	p1A(&stick, BUTTON1),//A
+	p1B(&stick, BUTTON2),//B
+	p1X(&stick, BUTTON3),//X
+	p1Y(&stick, BUTTON4),//Y
+	p1LB(&stick, BUTTON5),//LB
+	p1RB(&stick, BUTTON6),//RB
+	p1Back(&stick, BUTTON7),//Back
+	p1Start(&stick, BUTTON8),//Start
+	p1LStick(&stick, BUTTON9),//Left Stick
+	p1RStick(&stick, BUTTON10)//Right Stick
+{
+
+}
+
+float OI::getDriveRight(){
+	float raw = -stick.GetRawAxis(5);
+	if(raw < .115 && raw > -.115){
+			raw = 0;
+		}
+		return raw;
+}
+
+float OI::getDriveLeft(){
+	float raw = -stick.GetRawAxis(1);
+	if(raw < .115 && raw > -.115){
+		raw = 0;
+	}
+	return raw;
+}
